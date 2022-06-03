d4dd0f8594c0c3975577f60347ad9e31af8f0b38
Thiago Costa <thiagohrcosta86@gmail.com>
2022-06-02 21:13:06 -0300

    Adding aside and main content

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,21 @@ import { Header } from "./components/Header"
 
 import "./global.css";
 
+import styles from "./App.module.css";
+import { Post } from "./Post";
+
 function App() {
 
   return (
-   <Header />
+    <>
+      <Header />
+        <div className={styles.wrapper}>
+        <aside></aside>
+        <main>
+          <Post author="Thiago Costa" content="React and RoR" />
+        </main>
+        </div>
+    </>
   )
 }
 
