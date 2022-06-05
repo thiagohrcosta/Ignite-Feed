db7811e4634c84ec2563a2f573b18dd0e68a912d
thiagohrcosta <thiagohrcosta86@gmail.com>
2022-06-05 15:40:41 -0300

    Adding posts

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,23 @@ import styles from "./App.module.css";
 import { Sidebar } from "./components/Sidebar";
 import { Post } from "./components/Post";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUlr: "https://github.com/thiagohrcosta.png",
+      name: "Thiago Costa",
+      role: "Fullstack Developer RoR | ReactJS"
+    },
+    content:[
+    { type: 'paragraph', content:     'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+    { type: 'paragraph', content:     'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+    { type: 'link', content:     'github.com/thiagohrcosta'}
+    ],
+    publishedAt: newDate(Date.now() - 1),
+  },
+];
+
 function App() {
 
   return (
@@ -14,7 +31,11 @@ function App() {
         <div className={styles.wrapper}>
           <Sidebar />
         <main>
-          <Post />
+          {
+            posts.map(post => {
+              return <Post />
+            })
+          }
         </main>
         </div>
     </>
